adcc5eb54553c30bf515d2f9c6f5332255db2d3b
UN Thanks Developer <developer@example.com>
2025-05-15 14:12:49 +0900

    \Reduce API endpoints to fix Vercel deployment error\

--- api/200.js
+++
@@ -1,5 +0,0 @@
-// OPTIONS 요청 처리를 위한 간단한 핸들러
-module.exports = (req, res) => {
-  // 200 OK 응답 반환
-  res.status(200).end();
-};
--- api/simple-test.js
+++
@@ -1,7 +0,0 @@
-// 가장 간단한 테스트 API
-module.exports = async (req, res) => {
-  res.status(200).json({ 
-    message: 'Simple test works',
-    time: new Date().toISOString() 
-  });
-};
